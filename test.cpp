d57ac214de3ee214fc7f6c9056f4a0227453c600
mateu <mateu@touchiteasy.com>
2014-06-29 23:56:54 +0200

    Added type to throw checks

--- test.cpp
+++ test.cpp
@@ -9,26 +9,26 @@ int main(){
     Assert(true);
     assert.thatFn([]{
         Assert(false);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     Assert().that(1).equals(1);
     assert.thatFn([]{
         Assert().that(1).equals(2);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     Assert().that(true).equals(true);
     Assert().that(false).equals(false);
     assert.thatFn([]{
         Assert().that(false).equals(true);
-    }).throws();
+    }).throws<ExpectationMismatch>();
     assert.thatFn([]{
         Assert().that(true).equals(false);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     Assert().that("foo").equals("foo");
     assert.thatFn([]{
         Assert().that("foo").equals("bar");
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     []{
         string foo1 = "foo";
@@ -40,7 +40,7 @@ int main(){
         string foo = "foo";
         string bar = "bar";
         Assert().that(foo).equals(bar);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     []{
         string foo = "foo";
@@ -51,7 +51,7 @@ int main(){
         string foo = "foo";
         const char * bar = "bar";
         Assert().that(foo).equals(bar);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     []{
         string foo_string = "foo";
@@ -62,23 +62,23 @@ int main(){
     assert.thatFn([]{
         string bar = "bar";
         Assert().that("foo").equals(bar);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     assert.thatFn([]{
         Assert().that(1).equals(true);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     assert.thatFn([]{
         Assert().that(true).equals(1);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     assert.thatFn([]{
         Assert().that(0).equals(false);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     assert.thatFn([]{
         Assert().that(false).equals(0);
-    }).throws();
+    }).throws<ExpectationMismatch>();
 
     bool mismatchThrown;
 
